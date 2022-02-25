60f4c5094311cae234b6fc274531585840698273
Nick Mathew <nmathew.n75@gmail.com>
2022-02-25 10:12:04 -0500

    Added Encoders to the ARC, need to test them

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,10 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.Elevator.ElevatorSub;
-import frc.robot.Intake.IntakeSubsystem;
-import frc.robot.Shooter.subsystem.ShooterSubsytem;
-import frc.robot.Index.IndexSub;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
