0a44d4c9812e46aa22fc68c926200d207c6a5501
Caeden1 <95881703+Caeden1@users.noreply.github.com>
2022-03-07 14:27:37 -0500

    Update limelight.java

--- src/main/java/frc/robot/Limelight/limelight.java
+++ src/main/java/frc/robot/Limelight/limelight.java
@@ -15,6 +15,9 @@ public Limelight {
     
   }
   
+  public void update() {
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+  }
   //0 LED Mode set in the current pipeline
   //1 force off
   //2 force blink
